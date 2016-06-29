c0940374cdcdbae8f865365453ef42a31ca3ab42
Duve, Michael <mduve@designmail.net>
2016-06-29 12:34:42 +0200

    electron: building works

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+var argv = require('minimist')(process.argv.slice(2))["_"];
+
+const {
+    app
+} = electron;
+
+const {
+    BrowserWindow
+} = electron;
+
+const settings = {
+    debug: argv.indexOf("debug") > -1
+};
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 1200,
+        height: 600
+    });
+    win.loadURL(`file://${__dirname}/index.html`);
+    if (settings.debug) win.webContents.openDevTools();
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
